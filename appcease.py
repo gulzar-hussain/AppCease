01b274dce9a9165aa69b1670dec7861ff833184d
Gulzar Hussain <gh06210@st.habib.edu.pk>
2023-09-02 00:01:27 +0500

    Update appcease.py
    Multiple selections functionality + search option added

--- appcease.py
+++ appcease.py
@@ -7,39 +7,43 @@ import psutil
 class ProcessManagerApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Process Manager")
+        self.root.title("AppCease")
         self.allow_update = True  # Initialize the allow_update attribute
-        
-        self.process_listbox = tk.Listbox(root, selectmode=tk.SINGLE, width=50)
+
+        self.process_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE, width=50)
         self.process_listbox.grid(row=0, column=0, padx=10, pady=10, rowspan=4, sticky="nsew")  # Span rows and expand both directions
 
         self.process_scrollbar_y = tk.Scrollbar(root, orient=tk.VERTICAL, command=self.process_listbox.yview)
         self.process_listbox.config(yscrollcommand=self.process_scrollbar_y.set)
         self.process_scrollbar_y.grid(row=0, column=1, sticky="ns", rowspan=4, pady=10, ipadx=2)  # Span rows and adjust ipadx
 
+        self.search_label = tk.Label(root, text="Search:")
+        self.search_label.grid(row=4, column=0, padx=10, pady=5, sticky="w")  # Align label to the left
 
+        self.search_entry = tk.Entry(root)
+        self.search_entry.grid(row=4, column=0, padx=10, pady=5, sticky="ew")  # Align entry to the right
+        self.search_entry.bind('<KeyRelease>', self.update_search)
 
         self.update_process_list()
 
         self.update_button = tk.Button(root, text="Update List", command=self.update_process_list_manually)
-        self.update_button.grid(row=4, column=0, padx=10, pady=5, sticky="ew")  # Span columns
+        self.update_button.grid(row=5, column=0, padx=10, pady=5, sticky="ew")  # Span columns
 
         self.schedule_label = tk.Label(root, text="Enter schedule time (HH:MM):")
-        self.schedule_label.grid(row=5, column=0, padx=10, pady=5, sticky="w")  # Align label to the left
+        self.schedule_label.grid(row=6, column=0, padx=10, pady=5, sticky="w")  # Align label to the left
 
         self.schedule_entry = tk.Entry(root)
-        self.schedule_entry.grid(row=5, column=0, padx=10, pady=5, sticky="e")  # Align entry to the right
+        self.schedule_entry.grid(row=6, column=0, padx=10, pady=5, sticky="ew")  # Align entry to the right
 
-        self.schedule_button = tk.Button(root, text="Schedule Stop", command=self.schedule_process)
-        self.schedule_button.grid(row=5, column=1, padx=10, pady=5, sticky="e")  # Align button to the right
+        self.schedule_selected_button = tk.Button(root, text="Schedule Selected", command=self.schedule_selected_processes)
+        self.schedule_selected_button.grid(row=6, column=1, padx=10, pady=5, sticky="e")  # Align button to the right
 
         # Add the clock label and initialize it
         self.clock_label = tk.Label(root, text="", font=("Helvetica", 18))
-        self.clock_label.grid(row=6, column=0, padx=10, pady=5, columnspan=2, sticky="e")  # Align clock to the right
+        self.clock_label.grid(row=7, column=0, padx=10, pady=5, columnspan=2, sticky="e")  # Align clock to the right
         self.update_clock()  # Initialize the clock
 
         self.process_listbox.bind('<<ListboxSelect>>', self.toggle_update)
-
 
     def update_clock(self):
         current_time = datetime.now().strftime("%H:%M:%S")
@@ -62,7 +66,6 @@ class ProcessManagerApp:
         self.allow_update = True
         self.update_process_list()
 
-
     def get_user_applications(self):
         applications = []
         for proc in psutil.process_iter(attrs=['pid', 'name']):
@@ -74,15 +77,21 @@ class ProcessManagerApp:
                 pass
         return applications
 
-    def schedule_process(self):
-        selected_index = self.process_listbox.curselection()
-        if not selected_index:
-            messagebox.showerror("Error", "Please select an application to schedule.")
+    def update_search(self, event):
+        search_keyword = self.search_entry.get().lower()
+        applications = self.get_user_applications()
+        self.process_listbox.delete(0, tk.END)
+        for app in applications:
+            if search_keyword in app.lower():
+                self.process_listbox.insert(tk.END, app)
+
+    def schedule_selected_processes(self):
+        selected_indices = self.process_listbox.curselection()
+        if not selected_indices:
+            messagebox.showerror("Error", "Please select at least one application to schedule.")
             return
 
-        selected_app = self.process_listbox.get(selected_index)
-        pid = int(selected_app.split()[1])
-
+        selected_pids = [int(self.process_listbox.get(index).split()[1]) for index in selected_indices]
         schedule_time = self.schedule_entry.get()
         try:
             hours, minutes = map(int, schedule_time.split(":"))
@@ -91,10 +100,13 @@ class ProcessManagerApp:
             if scheduled_time < now:
                 scheduled_time += timedelta(days=1)  # Schedule for the next day if the time has passed
 
-            time_difference = scheduled_time - now 
+            time_difference = scheduled_time - now
             time_difference = int(time_difference.total_seconds() * 1000)
-            self.root.after(time_difference, lambda: self.stop_process(pid))
-            messagebox.showinfo("Scheduled", f"Application {pid} has been scheduled to stop at {scheduled_time}.")
+
+            for pid in selected_pids:
+                self.root.after(time_difference, lambda p=pid: self.stop_process(p))
+
+            messagebox.showinfo("Scheduled", f"Selected applications will be terminated at {scheduled_time}.")
         except ValueError:
             messagebox.showerror("Error", "Invalid time format. Please use HH:MM.")
 
@@ -102,9 +114,9 @@ class ProcessManagerApp:
         try:
             process = psutil.Process(pid)
             process.terminate()
+            messagebox.showinfo("Killed", "Terminated!")
         except psutil.NoSuchProcess:
             pass
-
 
 def main():
     if platform.system() == "Windows" or platform.system() == "Linux":
@@ -116,5 +128,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# TODO: search option - multiple program killer - feedback when prgram is killed - reset input field when program is killed
